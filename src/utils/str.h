e39db58515608424f6f3dcd2aed97a49932d923c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-01-28 15:20:12 +0100

    utils/str: Add string helpers.

---
+++ src/utils/str.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
+ */
+
+#ifndef _H_STR_
+#define _H_STR_
+
+#define str_safe(val) (val ? val : "")
+
+#endif /* _H_STR_ */
