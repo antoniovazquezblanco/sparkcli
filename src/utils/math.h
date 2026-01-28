eedb56fa520f3f40f54db3807da8352f59a644c7
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-01-28 15:19:50 +0100

    utils/math: Add simple math helpers.

---
+++ src/utils/math.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
+ */
+
+#ifndef _H_MATH_
+#define _H_MATH_
+
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
+
+#endif /* _H_MATH_ */
