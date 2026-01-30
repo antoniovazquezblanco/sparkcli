a675ceacf036f2aa44a7e020f516b1f9a4a90ee3
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-01-30 15:03:33 +0100

    utils/math: Fix warnings in Windows.

--- src/utils/math.h
+++ src/utils/math.h
@@ -7,7 +7,16 @@
 #ifndef _H_MATH_
 #define _H_MATH_
 
+#ifdef _WIN32
+#include <stdlib.h>
+#endif
+
+#ifndef min
 #define min(a, b) (a < b ? a : b)
+#endif
+
+#ifndef max
 #define max(a, b) (a > b ? a : b)
+#endif
 
 #endif /* _H_MATH_ */
